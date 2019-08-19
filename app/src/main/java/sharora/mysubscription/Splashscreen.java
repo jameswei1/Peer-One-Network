e73cc51077229e6b9e7520e9452c59d60f09cc17
James Wei <jameswei27@gmail.com>
2019-08-19 11:44:42 -0400

    Query working SMS message working Can notify for people who expire in 5 days and 1 day

--- app/src/main/java/sharora/mysubscription/Splashscreen.java
+++ app/src/main/java/sharora/mysubscription/Splashscreen.java
@@ -3,10 +3,17 @@ package sharora.mysubscription;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,9 +26,12 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import static sharora.mysubscription.AlarmReceiver.CHANNEL1_ID;
+
 public class Splashscreen extends AppCompatActivity {
     TextView name;
     Date date1;
+    private NotificationManagerCompat notificationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,23 +46,31 @@ public class Splashscreen extends AppCompatActivity {
         date1 = calendar.getTime();
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy");
         String strDate = dateFormat.format(date1);
-//        Log.d("date", strDate);
+        notificationManager = NotificationManagerCompat.from(this);
 
         Query query = FirebaseDatabase.getInstance().getReference("customers").orderByChild("Endsub").equalTo(strDate);
-
 
         ValueEventListener valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists() ){
-                    for(DataSnapshot ds: dataSnapshot.getChildren())
-                        name.setText( ds.getValue(Member.class).Getname());
+                if(dataSnapshot.exists()) {
+                    for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                        Log.d("id", "name");
+                        Toast.makeText(Splashscreen.this, "text", Toast.LENGTH_SHORT).show();
+                        //Getting intent and PendingIntent instance
+                        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                        PendingIntent pi= PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+
+                        //Get the SmsManager instance and call the sendTextMessage method to send message
+                        SmsManager sms=SmsManager.getDefault();
+                        sms.sendTextMessage("6478305885", null, "hello javatpoint", pi,null);
+                    }
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                Toast.makeText(Splashscreen.this, "Error occurred", Toast.LENGTH_SHORT).show();
             }
         };
         query.addListenerForSingleValueEvent(valueEventListener);
